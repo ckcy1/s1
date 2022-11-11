5eb141dc373dd41565db3563f8a9d668e4e1cebc
andy <ckcy1@qq.com>
2022-11-11 22:55:33 +0800

    连续成功

--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -1,9 +1,10 @@
 use std::net::TcpListener;
 fn main() {
-    let listener=TcpListener::bind("127.0.0.1:3003").unwrap();
+    let listener=TcpListener::bind("127.0.0.1:3000").unwrap();
     println!("Running on port 3000...");
     for stream in listener.incoming() {
-        let _stream =stream.unwrap();
-        print!("Connection established!");
+        let _stream = stream.unwrap();
+        println!("connection established")// 不要敲成print!("Connection established!")终端显示不了的
     }
 }
+
