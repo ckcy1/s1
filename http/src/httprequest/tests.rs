67660ad07a48857797941817dc7599a6c4db8961
andy <ckcy1@qq.com>
2022-11-12 21:12:36 +0800

    http

---
+++ http/src/httprequest/tests.rs
@@ -0,0 +1,27 @@
+use super::*;
+
+#[test]
+fn test_method_into(){
+    let m:Method="GET".into();
+    assert_eq!(m,Method::Get)
+}
+
+#[test]
+fn test_version_into(){
+    let v:Version="HTTP/1.1".into();
+    assert_eq!(v,Version::V1_1);
+}
+
+#[test]
+fn test_read_http(){
+    let s:String=String::from("GET/greeting HTTP/1.1\r\nHost: localhost:3000\r\nUser-Agent: curl/7.71.1\r\nAccept: */*\r\n\r\n");
+    let mut headers_expected = HashMap::new();
+    headers_expected.insert("Host".into(),"localhost".into());
+    headers_expected.insert("Accept".into(),"*/*".into());
+    headers_expected.insert("User-Agent".into(),"curl/7.71.1".into());
+    let req:HttpRequest=s.into();
+    assert_eq!(Method::Get,req.method);
+    assert_eq!(Version::V1_1,req.version);
+    assert_eq!(Resource::Path("/greeting".to_string()),req.resource);
+    assert_eq!(headers_expected,req.headers);
+}
